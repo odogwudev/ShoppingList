240c987751f3ea1f617cdb7586acac6c3b2ee047
ghostadventures <47299656+ghostadventures@users.noreply.github.com>
2021-03-01 18:37:42 +0100

    Testing Room Database

---
+++ app/src/main/java/com/odogwudev/shoppinglist/data/remote/PixabayAPI.kt
@@ -0,0 +1,18 @@
+package com.odogwudev.shoppinglist.data.remote
+
+import com.odogwudev.shoppinglist.BuildConfig
+import com.odogwudev.shoppinglist.data.remote.responses.ImageResponse
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface PixabayAPI {
+
+    @GET("/api/")
+    suspend fun searchImage(
+        @Query("q") searchQuery: String,
+        @Query("key") apiKey: String = BuildConfig.API_KEY
+    ): Response<ImageResponse>
+
+    
+}
---
+++ app/src/main/java/com/odogwudev/shoppinglist/di/AppModule.kt
@@ -0,0 +1,43 @@
+package com.odogwudev.shoppinglist.di
+
+import android.content.Context
+import androidx.room.Room
+import com.odogwudev.shoppinglist.data.local.ShoppingItemDatabase
+import com.odogwudev.shoppinglist.data.remote.PixabayAPI
+import com.odogwudev.shoppinglist.other.Constants.BASE_URL
+import com.odogwudev.shoppinglist.other.Constants.DATABASE_NAME
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(ApplicationComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideShoppingItemDatabase(
+        @ApplicationContext context: Context
+    ) = Room.databaseBuilder(context, ShoppingItemDatabase::class.java, DATABASE_NAME).build()
+
+    @Singleton
+    @Provides
+    fun provideShoppingDao(
+        database: ShoppingItemDatabase
+    ) = database.shoppingDao()
+
+    @Singleton
+    @Provides
+    fun providesPixabayApi(): PixabayAPI{
+        return Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(BASE_URL)
+            .build()
+            .create(PixabayAPI::class.java)
+    }
+}
