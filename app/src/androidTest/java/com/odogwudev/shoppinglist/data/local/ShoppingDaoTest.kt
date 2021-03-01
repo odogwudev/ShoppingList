b9749c7bb9d479543905ef23289713abf4d5722e
ghostadventures <47299656+ghostadventures@users.noreply.github.com>
2021-03-01 23:55:16 +0100

    Testing with Dagger-Hilt

--- app/src/androidTest/java/com/odogwudev/shoppinglist/data/local/ShoppingDaoTest.kt
+++ app/src/androidTest/java/com/odogwudev/shoppinglist/data/local/ShoppingDaoTest.kt
@@ -7,6 +7,9 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.SmallTest
 import com.google.common.truth.Truth.assertThat
 import com.odogwudev.shoppinglist.getOrAwaitValue
+import com.odogwudev.shoppinglist.ui.ShoppingFragment
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runBlockingTest
 import org.junit.After
@@ -14,36 +17,41 @@ import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import javax.inject.Inject
+import javax.inject.Named
 
 @ExperimentalCoroutinesApi
-@RunWith(AndroidJUnit4::class)
 @SmallTest
+@HiltAndroidTest
 class ShoppingDaoTest {
 
-    private lateinit var database: ShoppingItemDatabase
-    private lateinit var dao: ShoppingDao
+    @get:Rule
+    var hiltRule = HiltAndroidRule(this)
 
     @get:Rule
     var instantTaskExecutorRule = InstantTaskExecutorRule()
 
-    @Before
-    /** fun setup() {
-    database = Room.inMemoryDatabaseBuilder(
-    ApplicationProvider.getApplicationContext(),
-    //ShoppingItemDatabase::class.java
-    ).allowMainThreadQueries().build()
+    @Inject
+    @Named("test_db")
+    lateinit var database: ShoppingItemDatabase
+    private lateinit var dao: ShoppingDao
 
-    dao = database.shoppingDao()
+    @Before
+    fun setup() {
+        hiltRule.inject()
+        dao = database.shoppingDao()
     }
 
     @After
     fun teardown() {
-    database.close()
-    }*/
+        database.close()
+    }
+
+
 
     @Test
     fun insertShoppingItem() = runBlockingTest {
-        val shoppingItem = ShoppingItem("name", 400, 1f, "Url", id = 1)
+        val shoppingItem = ShoppingItem("name", 1, 1f, "url", id = 1)
         dao.insertShoppingItem(shoppingItem)
 
         val allShoppingItems = dao.observeAllShoppingItems().getOrAwaitValue()
@@ -53,25 +61,27 @@ class ShoppingDaoTest {
 
     @Test
     fun deleteShoppingItem() = runBlockingTest {
-        val shoppingItem = ShoppingItem("name", 400, 1f, "Url", id = 1)
+        val shoppingItem = ShoppingItem("name", 1, 1f, "url", id = 1)
         dao.insertShoppingItem(shoppingItem)
         dao.deleteShoppingItem(shoppingItem)
+        val allShoppingItem = dao.observeAllShoppingItems().getOrAwaitValue()
 
-        val allShoppingItems = dao.observeAllShoppingItems().getOrAwaitValue()
-        assertThat(allShoppingItems).doesNotContain(shoppingItem)
+        assertThat(allShoppingItem).doesNotContain(shoppingItem)
     }
 
     @Test
-    fun observeTotalPriceSum() = runBlockingTest {
-        val shoppingItem1 = ShoppingItem("name", 600, 10f, "Url", id = 1)
-        val shoppingItem2 = ShoppingItem("name", 400, 4f, "Url", id = 2)
-        val shoppingItem3 = ShoppingItem("name", 400, 5.5f, "Url", id = 3)
+    fun observeTotalPriceAmount() = runBlockingTest {
+        val shoppingItem1 = ShoppingItem("name", 2, 10f, "url", id = 1)
+        val shoppingItem2 = ShoppingItem("name", 4, 5.5f, "url", id = 2)
+        val shoppingItem3 = ShoppingItem("name", 0, 100f, "url", id = 3)
 
         dao.insertShoppingItem(shoppingItem1)
         dao.insertShoppingItem(shoppingItem2)
         dao.insertShoppingItem(shoppingItem3)
 
         val totalPriceSum = dao.observeTotalPrice().getOrAwaitValue()
-        assertThat(totalPriceSum).isEqualTo(600 * 10f + 400 * 4f + 400 * 5.5f)
+
+        assertThat(totalPriceSum).isEqualTo(2 * 10f + 4 * 5.5f)
+
     }
 }
